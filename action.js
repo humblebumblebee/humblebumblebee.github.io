0eab04135e1bd0c81a43bd022c9a42fc753db04a
mgharibi <humblebumblebee@users.noreply.github.com>
2020-06-21 16:05:53 -0400

    Update action.js

--- action.js
+++ action.js
@@ -2,8 +2,8 @@ callBackOnAction = (data) => {
   alert("Say data is check your console")
   window.postMessage(
     `{
-      Action: ${data.Action},
-      Target: ${data.Target},
+      Action: ` + data.Action + `,
+      Target: ` + data.Target + `},
      }`
   );
   console.log(data)
